6cc94bfbc8f9018cab2737117ecdd7f6fa9f8017
Ygnas <ygnas@outlook.com>
2024-01-25 11:04:17 +0000

    Delete listing function for listing controller

--- controllers/listings.go
+++ controllers/listings.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/Ygnas/FoodLog/models"
+	"github.com/go-chi/chi"
 	"github.com/google/uuid"
 )
 
@@ -42,13 +43,21 @@ func GetListing(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateListing(w http.ResponseWriter, r *http.Request) {
+	err := GetFirebaseDatabase().FirebaseConnect()
+	if err != nil {
+		http.Error(w, "Could not connect to Firebase", http.StatusInternalServerError)
+		return
+	}
+
 	var listing models.Listing
 
-	err := json.NewDecoder(r.Body).Decode(&listing)
+	err = json.NewDecoder(r.Body).Decode(&listing)
 	if err != nil {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
+
+	listing.ID = uuid.New()
 
 	storage := NewStorage()
 	err = storage.Create(&listing)
@@ -63,4 +72,23 @@ func CreateListing(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Write([]byte(responseJSON))
+}
+
+func DeleteListing(w http.ResponseWriter, r *http.Request) {
+	err := GetFirebaseDatabase().FirebaseConnect()
+	if err != nil {
+		http.Error(w, "Could not connect to Firebase", http.StatusInternalServerError)
+		return
+	}
+
+	id := chi.URLParam(r, "id")
+
+	storage := NewStorage()
+	err = storage.Delete(id)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("Listing deleted"))
 }
