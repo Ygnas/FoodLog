8da8496f99c392d54e3a3ce41973c0a949908da1
Ygnas <ygnas@outlook.com>
2024-02-22 11:01:09 +0000

    Util function to decode base64 to string

--- util/helper.go
+++ util/helper.go
@@ -5,3 +5,11 @@ import "encoding/base64"
 func Base64Encode(str string) string {
 	return base64.StdEncoding.EncodeToString([]byte(str))
 }
+
+func Base64Decode(str string) string {
+	data, err := base64.StdEncoding.DecodeString(str)
+	if err != nil {
+		return ""
+	}
+	return string(data)
+}
